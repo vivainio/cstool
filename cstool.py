42c4e46bafc788e5fa6236776423aa2b4e0ef850
Ville M. Vainio <ville.vainio@basware.com>
2015-01-17 17:03:21 +0200

    solution parsing (not complete)

--- cstool.py
+++ cstool.py
@@ -5,12 +5,17 @@ import pprint
 import yaml
 import args
 import copy
+from itertools import tee, ifilterfalse
+import re
 
 ns = "http://schemas.microsoft.com/developer/msbuild/2003"
 
 ET.register_namespace('',ns)
-class CsProj:
 
+def nss(s):
+	return s.replace('{}', '{%s}' % ns)
+
+class CsProj:
 	def collect(self):
 
 		# collect item groups
@@ -70,8 +75,49 @@ class CsProj:
 		self.itemgroup['Reference']['Include'] = [p.split(',')[0]
 			for p in self.itemgroup['Reference']['Include']]
 
+def absed(root, paths):
+	return [os.path.abspath(os.path.join(root, p)) for p in paths] 
 
+def reled(root, paths):
+	return [os.path.relpath(p, root) for p in paths] 
 
+def partition(pred, iterable):
+    'Use a predicate to partition entries into false entries and true entries'
+    # partition(is_odd, range(10)) --> 0 2 4 6 8   and  1 3 5 7 9
+    t1, t2 = tee(iterable)
+    return list(ifilterfalse(pred, t1)), filter(pred, t2)
+
+def msbuild_crawl(pth, depth):
+	p = MsBuild(pth)
+	p.collect()
+	prjs = absed(p.prjroot, p.projects)
+	leafs, edges = partition(lambda f: f.lower().endswith('xml'), prjs)
+	print " " * depth," ".join(reled(p.prjroot, leafs))
+	for leaf in leafs:
+		Solution(leaf)
+	for edge in edges:
+		msbuild_crawl(edge, depth + 1)
+
+class MsBuild:
+	def __init__(self, fname):
+		self.fname = fname
+		self.prjroot = os.path.dirname(fname)
+		self.t = ET.parse(fname)
+		self.r = root = self.t.getroot()
+		#print self.r
+	def collect(self):
+		hits = self.r.findall(nss(".//{}Target[@Name='Compile']/{}MSBuild" ))
+		self.projects = [h.attrib['Projects'] for h in hits]
+		#for h in hits:
+		#	print h.attrib
+		#print "msb", self.projects
+
+class Solution:
+	def __init__(self, fname):
+		self.fname = fname
+		parts = re.findall('Project(.*?) = (.*)$', open(fname).read(), re.MULTILINE)  
+		pts = parts[0][1]
+		print "Parts", pts
 
 def do_dump(arg):
 	for fname in arg.file:
@@ -108,6 +154,9 @@ def do_add(arg):
 
 	p.save("c:/t/test.csproj")
 
+def do_msbuild(arg):
+	p = msbuild_crawl(arg.file[0], 0)
+
 def main():
 	args.init()
 	s = args.sub('dump', do_dump)
@@ -117,6 +166,9 @@ def main():
 
 	s = args.sub('merge', do_merge)
 	s.arg('file', nargs='+')
+
+	s = args.sub('msbuild', do_msbuild)
+	s.arg('file', nargs=1)
 
 	args.parse()
 
